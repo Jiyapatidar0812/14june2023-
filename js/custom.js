15aad0e2bcf40b84bc2508229959daa3d183ce36
Jiyapatidar0812 <Jiyapatidar0812@gmail.com>
2023-06-15 16:52:02 +0530

    done

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+ 
+ //create a function 
+ //1.function definition /name define 
+ //function syntax
+ /* 
+ functioon <functionName>(){
+     //function body
+ }
+ */
+//1. Function definition
+//function definition is one time process
+ function greetMyStudents(name){  //I will receive the formal arguments
+    console.log("hello"+name);
+
+}
+//2.Invoke /call
+//Function calling is a many time process
+  greetMyStudents("garvit"); //we can pass some actual arguments
+  greetMyStudents(`jiya`);
+  greetMyStudents('jatin');
+  greetMyStudents(`lahar`);
+
+ 
+
+
---
+++ js/custom2.js
@@ -0,0 +1,15 @@
+
+//I will define the function name with camelCase
+//1. function definition is one time process
+function addTwoNumbers(n1,n2){
+    console.log(n1-8);
+    
+    
+
+}
+
+//2.function invoking/calling is many time process
+
+addTwoNumbers(20,30);
+addTwoNumbers(50,20);
+
